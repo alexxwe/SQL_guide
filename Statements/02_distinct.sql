f300d1a624f336ee20bf0c8fc8917b08fac083b0
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-07-02 22:05:11 +0200

    SQL guide

---
+++ Statements/02_distinct.sql
@@ -0,0 +1,5 @@
+-- Nos trae campos distintos y su valor
+
+SELECT distinct * FROM users
+
+SELECT distinct age FROM users
---
+++ Statements/03_where.sql
@@ -0,0 +1,6 @@
+que retorne lo que pedimos DONDE el valor cumpla con el criterio
+SELECT *  FROM users WHERE age = 15
+
+SELECT name  FROM users WHERE age >= 20
+
+SELECT distinct name, age FROM users WHERE age = 15
