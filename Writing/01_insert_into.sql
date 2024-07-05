1f1ed9104886b7fa4056a09d1f841d80b659b952
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-07-05 16:06:10 +0200

    SQL

---
+++ Writing/01_insert_into.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (user_id, name, surname) VALUE (8, 'Maria', 'Lopez')
+
+INSERT INTO users (name, surname) VALUE ('Paco', 'Perez')
+
+INSERT INTO users (user_id, name, surname) VALUE (11, 'Inazuma', 'Eleven')
---
+++ Writing/02_update.sql
@@ -0,0 +1,3 @@
+UPDATE users SET age = '21' WHERE user_id = 11
+
+UPDATE users SET age = '11', init_date = '2011-11-11' WHERE user_id = 11
